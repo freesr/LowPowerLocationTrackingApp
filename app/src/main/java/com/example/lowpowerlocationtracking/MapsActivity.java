f47b9fc82b22512749d730474ae50ef6a682b61b
freesr <evikas10020@gmail.com>
2023-05-09 01:09:23 -0400

    Final Version

--- app/src/main/java/com/example/lowpowerlocationtracking/MapsActivity.java
+++ app/src/main/java/com/example/lowpowerlocationtracking/MapsActivity.java
@@ -6,7 +6,6 @@ import androidx.fragment.app.FragmentActivity;
 
 import android.content.Context;
 import android.content.pm.PackageManager;
-import android.graphics.Color;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
@@ -23,11 +22,8 @@ import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
-import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
-import com.example.lowpowerlocationtracking.databinding.ActivityMapsBinding;
-import com.google.android.gms.maps.model.PolylineOptions;
 import com.google.maps.android.SphericalUtil;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback, LocationListener, View.OnClickListener, SensorEventListener {
@@ -35,24 +31,20 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     private GoogleMap mMap;
     private LocationManager locationManager;
     private Button currentLocationBtn, plusSignBtn, minusSignBtn;
-    float acclX = 0, acclY = 0, acclZ = 0;
     private SensorManager sensorManager;
     private Sensor accelerometerSensor;
-    float acceleration = 0;
-    //float velocity = 0;
-    float displacement = 0;
-    float timeStep = 0.01f; // time step in seconds
-    long lastUpdate = System.currentTimeMillis();
-
-    final float alpha = 0.8f;
-    float[] gravity = {0, 0, 0};
+    final float alpha = 0.01f;
 
     // Initialize the displacement variables
-    float distanceTraveled = 0;
-    float[] velocity = {0, 0, 0};
-    float[] position = {0, 0, 0};
+    float[] sensorPosition = {0, 0, 0};
+    float sensorDistance = 0;
+    float[] sensorGravity = {0, 0, 0};
+    float[] sensorVelocity = {0, 0, 0};
+    float differTime;
+
     long lastTime = System.currentTimeMillis();
-    long oldTime =0;
+    long oldTime = 0;
+    long presentTime;
 
     LatLng latLng;
     int zoom = 16;
@@ -77,7 +69,6 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.mapView);
         mapFragment.getMapAsync(this);
-       // latLng = new LatLng(location.getLatitude(), location.getLongitude());
     }
 
     @Override
@@ -101,17 +92,9 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
         latLng = new LatLng(39.261, -76.699);
-        mMap.addMarker(new MarkerOptions().position(latLng).title("Random Point"));
+        mMap.addMarker(new MarkerOptions().position(latLng).title("Inital Point"));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(latLng));
-//        mMap.setOnCamaeraChangeListener(new GoogleMap.OnCameraChangeListener() {
-//            @Override
-//            public void onCameraChange(@NonNull CameraPosition cameraPosition) {
-//                if (cameraPosition.zoom != zoom) {
-//                    zoom = (int) cameraPosition.zoom;
-//                    // do you action here
-//                }
-//            }
-//        });
+        Toast.makeText(getApplicationContext(), "click current location again", Toast.LENGTH_LONG).show();
     }
 
 
@@ -135,12 +118,12 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                 return;
             }
             locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 5000, 0, this);
-            if(latLng != null){
-                mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng, 15));
+            if (latLng != null) {
+                setZoom();
             }
-            sensorManager.registerListener((SensorEventListener) MapsActivity.this,accelerometerSensor,SensorManager.SENSOR_DELAY_NORMAL);
-        } else if(v.getId() == R.id.plusSign){
-            if(latLng != null){
+            sensorManager.registerListener((SensorEventListener) MapsActivity.this, accelerometerSensor, SensorManager.SENSOR_DELAY_NORMAL);
+        } else if (v.getId() == R.id.plusSign) {
+            if (latLng != null) {
                 zoom = zoom + 3;
                 setZoom();
             }
@@ -163,77 +146,91 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     }
 
     private void setZoom() {
+
         mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng, zoom));
     }
 
     @Override
     protected void onResume() {
         super.onResume();
+//        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+//            // TODO: Consider calling
+//            //    ActivityCompat#requestPermissions
+//            // here to request the missing permissions, and then overriding
+//            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+//            //                                          int[] grantResults)
+//            // to handle the case where the user grants the permission. See the documentation
+//            // for ActivityCompat#requestPermissions for more details.
+//            return;
+//        }
+//        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 6000, 0,this);
+//        sensorManager.registerListener((SensorEventListener) MapsActivity.this, accelerometerSensor, SensorManager.SENSOR_DELAY_NORMAL);
+
     }
+
+
 
     @Override
     public void onLocationChanged(@NonNull Location location) {
         mMap.clear();
         latLng = new LatLng(location.getLatitude(), location.getLongitude());
-        mMap.addMarker(new MarkerOptions().position(latLng).title("New Location"));
-        //setZoom();
-        //        gravity = new float[]{0, 0, 0};
-//
-//        // Initialize the displacement variables
-//        distanceTraveled = 0;
-//         velocity = new float[]{0, 0, 0};
-//         position = new float[]{0, 0, 0};
-//        mMap.clear();
-//        mMap.addMarker(new MarkerOptions().position(latLng).title("New Location"));
-//        setZoom();
+        mMap.addMarker(new MarkerOptions().position(latLng).title("GPS Location"));
+        sensorGravity = new float[]{0, 0, 0};
+         sensorPosition = new float[]{0, 0, 0};
+       sensorDistance = 0;
+        sensorGravity = new float[]{0, 0, 0};
+        sensorVelocity = new float[]{0, 0, 0};
+
     }
 
     @Override
     public void onSensorChanged(SensorEvent event) {
         if(event.sensor.getType() == Sensor.TYPE_ACCELEROMETER){
 
-            gravity[0] = alpha * gravity[0] + (1 - alpha) * event.values[0];
-            gravity[1] = alpha * gravity[1] + (1 - alpha) * event.values[1];
-            gravity[2] = alpha * gravity[2] + (1 - alpha) * event.values[2];
-            float[] linearAcceleration = {
-                    event.values[0] - gravity[0],
-                    event.values[1] - gravity[1],
-                    event.values[2] - gravity[2]
+//            sensorGravity[0] = alpha * sensorGravity[0] + (1 - alpha) * event.values[0];
+//            sensorGravity[1] = alpha * sensorGravity[1] + (1 - alpha) * event.values[1];
+//            sensorGravity[2] = alpha * sensorGravity[2] + (1 - alpha) * event.values[2];
+
+            float[] filteredAcceleration = {
+//                    event.values[0] - sensorGravity[0],
+//                    event.values[1] - sensorGravity[1],
+//                    event.values[2] - sensorGravity[2]
+                    event.values[0] ,
+                    (float) (event.values[1] - 9.8),
+                    event.values[2]
             };
 
             // Calculate the time elapsed since the last sensor reading
-            long currentTime = System.currentTimeMillis();
-            float dt = (currentTime - lastTime) / 1000.0f;
-            lastTime = currentTime;
+            presentTime = System.currentTimeMillis();
+            differTime = (presentTime - lastTime) / 1000.0f;
+            lastTime = presentTime;
 
             // Integrate the linear acceleration to estimate the velocity and displacement
             for (int i = 0; i < 3; i++) {
-                velocity[i] += linearAcceleration[i] * dt;
-                position[i] += velocity[i] * dt + 0.5f * linearAcceleration[i] * dt * dt;
+                sensorVelocity[i] += filteredAcceleration[i] * differTime;
+                sensorPosition[i] += sensorVelocity[i] * differTime + 1/2 * filteredAcceleration[i] * Math.pow(differTime,2);
             }
 
             // Update the distance traveled variable
-            distanceTraveled = (float) Math.sqrt(position[0] * position[0] + position[1] * position[1] + position[2] * position[2]);
-            distanceTraveled = distanceTraveled/10;
-            System.out.println("distance Travelled"+ distanceTraveled);
-                          //  float[] results = new float[1];
-//            Location.distanceBetween(latLng.latitude, latLng.longitude,
-//                        latLng.latitude + distanceTraveled, latLng.longitude, results);
-//                float distance = results[0];
+            sensorDistance = (float) Math.sqrt(Math.pow(sensorPosition[0],2) + Math.pow(sensorPosition[1],2) + Math.pow(sensorPosition[2],2));
+            sensorDistance = sensorDistance /1000;
+
                 if(oldTime ==0){
-                    oldTime = currentTime;
+                    oldTime = presentTime;
                 }
-               if(currentTime - oldTime > 1000) {
-                   System.out.println("Hiii      ");
-                   oldTime =currentTime;
-                   LatLng newLatLng = SphericalUtil.computeOffset(latLng, distanceTraveled, 0);
+               //if(presentTime - oldTime > 500) {
+                   oldTime =presentTime;
+            float[] results = new float[1];
+            Location.distanceBetween(latLng.latitude, latLng.longitude,
+                    latLng.latitude + sensorDistance, latLng.longitude+sensorDistance, results);
+            float distance = results[0];
+                   LatLng newLatLng = SphericalUtil.computeOffset(latLng, distance, 0);
                    System.out.println("lat" + newLatLng.latitude);
+                   System.out.println("distance Travelled"+ sensorDistance);
                    mMap.clear();
                    mMap.addMarker(new MarkerOptions().position(newLatLng));
-                  // mMap.addPolyline(new PolylineOptions().add(latLng, newLatLng).width(5).color(Color.RED));
-                   //latLng = newLatLng;
-                   setZoom();
-               }
+                  // setZoom();
+             //  }
         }
 
     }
